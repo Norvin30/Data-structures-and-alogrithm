1576fedb6b5bf8610d0b7015e84e5290182bf40b
Norvin30 <108176736+Norvin30@users.noreply.github.com>
2023-11-04 16:06:10 +0530

    Add files via upload

---
+++ linkedlist.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class linkedlist {
+    public static void main(String[] args) {
+
+        LinkedList<String> linkedList = new LinkedList<String>();
+
+        // push method is of adding a element into the stack.linkedlist is used with stack.
+
+        linkedList.push("A");
+        linkedList.push("B");
+        linkedList.push("C");
+        linkedList.push("D");
+
+        linkedList.add(3,"X");
+
+        System.out.println(linkedList);
+
+        // offer method is of queue. here linkedlist is used with queue.
+
+        linkedList.offer("S");
+        linkedList.offer("Z");
+        linkedList.offer("V");
+        linkedList.offer("N");
+
+        linkedList.add(3,"Q");
+
+        System.out.println(linkedList.peekFirst());
+
+        System.out.println(linkedList.peekLast());
+
+        linkedList.addFirst("0"); // addFirst method is used to add element to the head of linked list.
+        linkedList.addLast("1"); // addLast method is used to remove element of the tail of linked list.
+
+        String first = linkedList.removeFirst(); // removeFirst method is used to remove element which is the head of linked list.
+        String last = linkedList.removeLast(); // removeLast method is used to remove element on the tail of linkedlist.
+
+        System.out.println(linkedList);
+    }
+}
