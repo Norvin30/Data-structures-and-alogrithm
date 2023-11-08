82b5dc1ea61ece1398ec55cd0754e92d1bcbf8d4
Norvin30 <108176736+Norvin30@users.noreply.github.com>
2023-11-08 18:50:46 +0530

    Add files via upload

---
+++ bubblesort.java
@@ -0,0 +1,21 @@
+public class bubblesort {
+    public static void main(String[] args){
+         int[] array ={3,6,9,5,8,1,4,2,7};
+
+         bubbleSort(array);
+         for (int i : array){
+             System.out.println(i);
+         }
+    }
+    public static void bubbleSort(int array[]){
+        for(int i = 0; i < array.length - 1; i++){
+            for (int j = 0; j < array.length - 1; j++){
+                if (array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] =  array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+}
