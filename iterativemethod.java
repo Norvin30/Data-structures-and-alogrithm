94a80de7cfe7029169d0cf89388cece681592f30
Norvin30 <108176736+Norvin30@users.noreply.github.com>
2023-11-10 09:28:49 +0530

    Add files via upload

---
+++ iterativemethod.java
@@ -0,0 +1,11 @@
+public class iterativemethod {
+    public static void main(String[] args){
+        walk(7);
+    }
+// Iteration is repeation of a process.
+    private static void walk(int step) {
+        for (int i = 0; i < step; i++){
+            System.out.println("You should take a step");
+        }
+    }
+}
