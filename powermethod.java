89006ff547765d417f3b03eedb441c55e2201b43
Norvin30 <108176736+Norvin30@users.noreply.github.com>
2023-11-13 20:32:53 +0530

    Add files via upload

---
+++ powermethod.java
@@ -0,0 +1,10 @@
+public class powermethod {
+    public static void main(String[] args) {
+        System.out.println(power(2, 14));
+    }
+
+    private static int power(int base, int exponent) {
+        if (exponent < 1) return 1;
+        return base * power(base, exponent - 1);
+    }
+}
